f42a65200e1afb405eba4776b9da8fafccb6db76
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-11-01 02:10:57 +0530

    Updated db

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
 
 dotenv.config();
 
@@ -8,9 +10,13 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, "ca.pem")), // <-- Aiven CA certificate
+    },
 });
 
 module.exports = db;
